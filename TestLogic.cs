f47d506f58af1a4a5d2febd6abbbdc5c48263dd0
Rob <robert.p.webb@live.com>
2017-09-07 19:33:06 +0100

    Updates

--- TestLogic.cs
+++ TestLogic.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class TestLogic : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+    private GameObject[] civs;
+	void Start ()
+	{
+        civs = new GameObject[GameObject.FindGameObjectsWithTag("Civillian").Length];
+	    civs = GameObject.FindGameObjectsWithTag("Civillian");
+	    for (int i = 0; i < civs.Length; i++)
+	    {
+	        if (civs[i].GetComponent<CivillianControl>() != null)
+	        {
+	            civs[i].GetComponent<CivillianControl>().NavAgent.avoidancePriority =
+	                civs[i].GetComponent<CivillianControl>().NavAgent.avoidancePriority += i;
+
+	        }
+	    }
 	}
 	
 	// Update is called once per frame
@@ -17,6 +29,8 @@ public class TestLogic : MonoBehaviour {
             int randomNumber = Random.Range(1, 3);
             Debug.Log(randomNumber);
         }
+
+
 		
 	}
 }
